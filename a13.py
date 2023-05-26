7f4b7d9a03da7791d4eaf5d72b4e402f0cf0d564
Tamicktom <rickpokemon38@gmail.com>
2023-05-26 20:10:51 -0300

    first commit

---
+++ a13.py
@@ -0,0 +1,12 @@
+
+
+nome = "Luiz Otávio"
+altura = 1.8
+peso = 95
+imc = peso / altura ** 2
+
+linha_1 = f"{nome} tem {altura} de altura e pesa {peso}Kg"
+linha_2 = f"pesa {peso}Kg e seu IMC é"
+linha_3 = f"{imc:.2f}"
+
+print(linha_1, linha_2, linha_3)
