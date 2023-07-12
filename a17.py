86aef5ae8411e0d864d7fabf7aebbb6c8a199128
Tamicktom <rickpokemon38@gmail.com>
2023-07-12 13:44:59 -0300

    Create a17.py

---
+++ a17.py
@@ -0,0 +1,8 @@
+condicao = True
+
+if condicao:
+    print("Este é o código que será executado se a condição for verdadeira")
+else:
+    print("Este é o código que será executado se a condição for falsa")
+
+print("Este é o código que será executado independente da condição")
