6e5de8c0d8b2037139ac70da82738b9a9c8b9ef6
Tamicktom <rickpokemon38@gmail.com>
2023-07-12 10:58:11 -0300

    saving

--- a14.py
+++ a14.py
@@ -4,8 +4,8 @@ c: float = 1.1123123
 formato: str = "a={} b={} c={}".format(a, b, c)
 formato2: str = "a={2:.2f} b={0} c={1}".format(a, b, c)
 
-formato3: str = "a={nome3:.2f} b={0} c={1}"
+formato3: str = "a={nome1} b={nome2} c={nome3}"
 
 print(formato)
 print(formato2)
-print(formato3.format(1, 2, nome3=3.1415))
+print(formato3.format(nome1=a, nome2=b, nome3=c.__round__(2)))
