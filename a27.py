70500b6d6086bcf3ef3340d710c47f9c33ea4893
Tamicktom <rickpokemon38@gmail.com>
2023-07-13 16:40:59 -0300

    Create a27.py

---
+++ a27.py
@@ -0,0 +1,10 @@
+"""
+Fatiamento de strings
+
+variavel[inicio:fim:passo]
+"""
+#           0123456789.........
+variavel = "Luiz Otávio Miranda"
+print(variavel[0:4]) # Luiz
+print(variavel[5:8]) # Otá
+print(variavel[9::2]) # i iad
