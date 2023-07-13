75a8f2c67210f69cb974bddba90b1e053f22e794
Tamicktom <rickpokemon38@gmail.com>
2023-07-13 16:32:52 -0300

    Create a26.py

---
+++ a26.py
@@ -0,0 +1,8 @@
+"""
+Formatação básica de strings
+"""
+
+variavel = "ABC"
+print(f"{variavel: >10}") # 10 espaços a direita
+print(f"{variavel: <10}") # 10 espaços a esquerda
+print(f"{variavel: ^10}") # 10 espaços centralizado
