4fd88de6d359d08e75f644a24b3f60808f000e14
Tamicktom@ <rickpokemon38@gmail.com>
2023-07-23 15:06:14 -0300

    Update a32ex1.py

--- a32ex1.py
+++ a32ex1.py
@@ -8,7 +8,8 @@ numero = input("Digite um número inteiro: ")
 
 try:
     numero = int(numero)
-    if numero % 2 == 0:
+    e_par = numero % 2 == 0
+    if (e_par):
         print(f"O número {numero}, é par.")
     else:
         print(f"O número {numero} é ímpar.")
