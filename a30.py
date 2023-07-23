6ed6e2b4df4f10f1e1dea8f274510b5b7513aced
Tamicktom@ <rickpokemon38@gmail.com>
2023-07-23 12:52:31 -0300

    Create a30.py

---
+++ a30.py
@@ -0,0 +1,11 @@
+"""
+CONSTANTE = "Variáveis" que não vão mudar de valor
+"""
+
+valocidade = 61  # Valocidade atual do carro
+local_carro = 101  # local em que o carro está na estrada
+
+RADAR_1 = 60  # Velocidade máxima permitida no radar 1
+LOCAL_1 = 100  # Local do radar 1
+RADAR_RANGE = 1  # A distância onde o radar pega
+
