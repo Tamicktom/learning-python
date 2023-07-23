6e950191fc0a896e401a5b07c7697a432a496e33
Tamicktom@ <rickpokemon38@gmail.com>
2023-07-23 15:23:49 -0300

    add more classes

---
+++ a33.py
@@ -0,0 +1,9 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imútaveis: str, int, float, bool
+"""
+
+string = "Luiz Ótávio"
+outra_string = f"{string[:4]} é legal"
+
+print(outra_string)
---
+++ a34.py
@@ -0,0 +1,12 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+loop infinito -> Quando um código não tem fim
+"""
+
+condicao = True
+
+while condicao:
+    nome = input('Qual o seu nome? ')
+    print(f'Olá {nome}!')
