a7c2aa9f934730917ad7defa6052075452671093
Henrique Fonseca <rickpokemon38@gmail.com>
2023-12-25 15:14:25 -0300

    Update user authentication logic

---
+++ aulas/sessao1/a78.py
@@ -0,0 +1,19 @@
+
+
+set1 = set()
+
+set1.add(1)
+set1.add(2)
+set1.add(3)
+set1.add(2)
+
+print(set1)
+
+set2 = set()
+
+set2.add("limão")
+set2.add("laranja")
+set2.add("maçã")
+set2.add("laranja")
+
+print(set2)
