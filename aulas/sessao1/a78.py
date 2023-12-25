8a20e477bacfd94ea98df8ee0964418ea964f2e1
Henrique Fonseca <rickpokemon38@gmail.com>
2023-12-25 15:30:00 -0300

    Add set operations to a78.py

--- aulas/sessao1/a78.py
+++ aulas/sessao1/a78.py
@@ -20,3 +20,15 @@ set2.update(set1) # Adiciona os elementos de set1 em set2
 set2.discard(2) # Remove o elemento 2 de set2
 
 print(set2)
+
+set3 = set1.union(set2) # Cria um novo conjunto com os elementos de set1 e set2
+set4 = set1.intersection(set2) # Cria um novo conjunto com os elementos que estão em set1 e set2
+set5 = set1.difference(set2) # Cria um novo conjunto com os elementos que estão em set1 e não estão em set2
+
+set3method2 = set1 | set2 # Cria um novo conjunto com os elementos de set1 e set2
+set4method2 = set1 & set2 # Cria um novo conjunto com os elementos que estão em set1 e set2
+set5method2 = set1 - set2 # Cria um novo conjunto com os elementos que estão em set1 e não estão em set2
+
+print(set3)
+print(set4)
+print(set5)
